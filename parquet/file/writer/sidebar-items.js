aacb23bddd16f036738eccf78628dd5bdd317fc5
alamb <alamb@users.noreply.github.com>
2024-06-21 15:59:21 +0000

    deploy: 3930d5b056b8157e0b5c5eb01b1076e476acf99b

--- parquet/file/writer/sidebar-items.js
+++ parquet/file/writer/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["SerializedColumnWriter","SerializedFileWriter","SerializedPageWriter","SerializedRowGroupWriter","TrackedWrite"],"type":["OnCloseColumnChunk","OnCloseRowGroup"]};
+window.SIDEBAR_ITEMS = {"fn":["write_bloom_filters"],"struct":["SerializedColumnWriter","SerializedFileWriter","SerializedPageWriter","SerializedRowGroupWriter","TrackedWrite"],"type":["OnCloseColumnChunk","OnCloseRowGroup"]};
