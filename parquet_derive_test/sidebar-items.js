4f5a774ea3f0bb80a290064a061e46d4dee928a9
alamb <alamb@users.noreply.github.com>
2024-07-01 10:30:33 +0000

    deploy: 62c1615e8ff822f5aa3adbc1e0acb1b5c59ea2a9

--- parquet_derive_test/sidebar-items.js
+++ parquet_derive_test/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["ACompleteRecord","APartiallyCompleteRecord"]};
+window.SIDEBAR_ITEMS = {"struct":["ACompleteRecord","APartiallyCompleteRecord","APartiallyOptionalRecord"]};
